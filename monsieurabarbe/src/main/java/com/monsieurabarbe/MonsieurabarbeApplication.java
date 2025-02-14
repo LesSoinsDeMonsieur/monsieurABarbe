02ccc130dfed98663b163b2faf875d2e822e3112
Jules CROIZIER <jules.croizier@gmail.com>
2025-02-14 10:07:33 +0100

    inital commit

---
+++ monsieurabarbe/src/main/java/com/monsieurabarbe/MonsieurabarbeApplication.java
@@ -0,0 +1,13 @@
+package com.monsieurabarbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonsieurabarbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonsieurabarbeApplication.class, args);
+	}
+
+}
---
+++ monsieurabarbe/src/test/java/com/monsieurabarbe/MonsieurabarbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monsieurabarbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonsieurabarbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
