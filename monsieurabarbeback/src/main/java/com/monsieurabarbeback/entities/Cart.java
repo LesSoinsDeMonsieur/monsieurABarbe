61ae60d2b4e6d313451648032a81d5a91358d38c
Jules CROIZIER <jules.croizier@gmail.com>
2025-03-21 10:39:45 +0100

    feature:adding first step CRUD

---
+++ monsieurabarbeback/src/main/java/com/monsieurabarbeback/entities/Cart.java
@@ -0,0 +1,30 @@
+package com.monsieurabarbeback.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "carts")
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
+    private Set<CartItem> cartItems = new HashSet<>();
+
+    public Cart(User user) {
+        this.user = user;
+    }
+}
---
+++ monsieurabarbeback/src/main/java/com/monsieurabarbeback/repositories/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.monsieurabarbeback.repositories;
+
+import com.monsieurabarbeback.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    boolean existsByName(String name);
+    List<Product> findByName(String name);
+}
