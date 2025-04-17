22f1ab560300c12f211696fcd5a7cfa224ddcd60
badaboom21 <130473771+badaboom21@users.noreply.github.com>
2025-04-17 15:31:26 +0200

    fix css

--- monsieurABarbeFront/src/app/page.tsx
+++ monsieurABarbeFront/src/app/page.tsx
@@ -116,35 +116,6 @@ export default function Home() {
           <a href="">JE DÉCOUVRE</a>
         </div>
       </div>
-      {/* </main> */}
-      {/* <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
-          Go to nextjs.org →
-        </a>
-      </footer> */}
-      {/* </div>
-      </div> */}
       <div className={styles.tutoriel}>
         <h1>Comment ça marche</h1>
         <div className={styles.etapes}>
@@ -152,10 +123,10 @@ export default function Home() {
             {
               <Image
                 className={styles.logo}
-                src="/Produits-cosmetiques.jpg"
+                src="/tuto_etape1.png"
                 alt="Vercel logomark"
-                width={20}
-                height={20}
+                width={200}
+                height={200}
               />
             }
             <h2>1</h2>
@@ -166,10 +137,10 @@ export default function Home() {
             {
               <Image
                 className={styles.logo}
-                src="/Produits-cosmetiques.jpg"
+                src="/tuto_etape2.png"
                 alt="Vercel logomark"
-                width={20}
-                height={20}
+                width={300}
+                height={300}
               />
             }
             <h2>2</h2>
@@ -180,10 +151,10 @@ export default function Home() {
             {
               <Image
                 className={styles.logo}
-                src="/Produits-cosmetiques.jpg"
+                src="/tuto_etape3.png"
                 alt="Vercel logomark"
-                width={20}
-                height={20}
+                width={200}
+                height={200}
               />
             }
             <h2>3</h2>
