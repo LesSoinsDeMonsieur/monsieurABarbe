f25010ceadea888f5cab57edfb5fd608cc63e2ec
badaboom21 <130473771+badaboom21@users.noreply.github.com>
2025-04-26 17:36:20 +0200

    suppression de fichiers inutiles

--- monsieurABarbeFront/src/app/layout.tsx
+++ monsieurABarbeFront/src/app/layout.tsx
@@ -3,8 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { AuthProvider } from "@/contexts/AuthContext";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
 import styles from "./page.module.css";
 
 const geistSans = Geist({
