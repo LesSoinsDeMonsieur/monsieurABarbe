06f53c11155fa04090223a562f520d423a28ce5f
badaboom21 <130473771+badaboom21@users.noreply.github.com>
2025-05-04 16:41:56 +0200

    initialisation de la page profil

---
+++ monsieurABarbeFront/src/app/profil/FormProfil.tsx
@@ -0,0 +1,160 @@
+"use client";
+
+import { useState } from "react";
+import styles from "./profil.module.css";
+import { AuthStatus, useAuth } from "@/contexts/AuthContext";
+import { authStatusToString } from "@/utils/enumToString";
+import { redirect } from "next/navigation";
+
+import axios from "axios";
+import axiosI from "@/axiosInterceptor";
+
+export default function FormProfil() {
+  const [prenom, setPrenom] = useState<string>("");
+  const [nom, setNom] = useState<string>("");
+  const [userName, setUserName] = useState<string>("");
+  const [biographie, setBiographie] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const { submitRegister } = useAuth();
+  const [error, setError] = useState<string | null>(null);
+  const [profil, setProfil] = useState<UserProfil | null>(null);
+
+  type UserProfil = {
+    prenom: string;
+    nom: string;
+    biographie: string;
+    userName: string;
+  };
+
+  const fetchProfil = async () => {
+    try {
+      const response = await axios.get<UserProfil>("http://localhost:8080/api/profil", {
+        // headers: {
+        //   Authorization: `Bearer ${token}`,
+        // },
+      });
+      setProfil(response.data);
+      if (profil != null) {
+        setPrenom(profil.prenom);
+        setNom(profil.nom);
+        setUserName(profil.userName);
+        setBiographie(profil.biographie);
+      }
+    } catch (err) {
+      console.error(err);
+      setError("Impossible de récupérer le panier.");
+    }
+  };
+
+  //post maj profil
+  async function profilRequest(user: UserProfil) {
+    return await axiosI.post("/auth/profil", {
+      prenom: user.prenom,
+      nom: user.nom,
+      bio: user.biographie,
+      username: user.userName,
+    });
+  }
+
+  const submitUpdateProfil = async (user: UserProfil): Promise<AuthStatus> => {
+    try {
+      const result = await profilRequest({ ...user });
+      if (!result) {
+        return AuthStatus.ERROR;
+      }
+      return AuthStatus.OK;
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        if (err.request.status === 400) {
+          //Wrong credentials
+          return AuthStatus.INVALID_FORMAT;
+        }
+        if (err.request.status === 409) {
+          //Email already used
+          return AuthStatus.LOGIN_UNAVAILABLE;
+        }
+      }
+      return AuthStatus.ERROR;
+    }
+  };
+
+  const submit = async () => {
+    if (prenom && nom && userName && biographie /*&& password*/) {
+      const status = await submitUpdateProfil({ prenom, nom, biographie, userName }); // MODIFIER POUR QUE CA MODIFIE JUSTE LES DONNEES DU USER
+      if (status != AuthStatus.OK) {
+        setErrorMessage(authStatusToString(status));
+        return;
+      }
+    } else {
+      setErrorMessage("Veuillez remplir tous les champs");
+      return;
+    }
+    redirect("/");
+  };
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        submit();
+      }}
+      // action={""}
+      className={styles.formLogin}
+    >
+      <div className="">
+        <input
+          type="text"
+          className={styles.formInputLogin}
+          placeholder="Prénom"
+          value={prenom}
+          onChange={(e) => setPrenom(e.target.value)}
+          autoFocus
+          required
+        />
+      </div>
+      <div className="">
+        <input
+          type="text"
+          className={styles.formInputLogin}
+          placeholder="Nom"
+          value={nom}
+          onChange={(e) => setNom(e.target.value)}
+          autoFocus
+          required
+        />
+      </div>
+      <div className="">
+        <input
+          type="text"
+          className={styles.formInputLogin}
+          placeholder="UserName"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+          autoFocus
+          required
+        />
+      </div>
+      <div className="">
+        <input
+          type="text"
+          className={styles.formInputLogin}
+          placeholder="Bio"
+          value={biographie}
+          onChange={(e) => setBiographie(e.target.value)}
+          autoFocus
+          required
+        />
+      </div>
+      {errorMessage && <div className="">{errorMessage}</div>}
+
+      <button type="submit" className={styles.formButtonLogin}>
+        Sauvegarder
+      </button>
+
+      <div className="">
+        <div onClick={() => redirect("/")} className={styles.formAuthMethod}>
+          Annuler
+        </div>
+      </div>
+    </form>
+  );
+}
---
+++ monsieurABarbeFront/src/app/profil/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./profil.module.css";
+import FormProfil from "./FormProfil";
+
+export default function Page() {
+  return (
+    <div className={styles.loginBody}>
+      <h2 className="">Votre Profil</h2>
+      <FormProfil />
+    </div>
+  );
+}
