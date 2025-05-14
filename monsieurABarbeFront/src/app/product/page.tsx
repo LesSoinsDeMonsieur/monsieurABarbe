b81b6e01b64e4b12d84b68f480ebb45a2e02496f
badaboom21 <130473771+badaboom21@users.noreply.github.com>
2025-05-14 10:53:44 +0200

    Avancé dans la fiche produit

---
+++ monsieurABarbeFront/src/app/product/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import PageProduit from "./pageProduct";
+import { useState } from "react";
+
+interface ProductType {
+  name: string;
+  description: string;
+  price: string;
+  stock: string;
+  imageUrl: string;
+}
+
+export default function Page() {
+  const [products] = useState<ProductType[]>([
+    {
+      name: "Crème Hydratante Pour Barbe Burberry",
+      description:
+        "Cette crème vous fera sentir comme un pélican. Un magnifique étalon dont les cheveux seront aussi doux que la crinière d'un cheval sauvage.<br />Je pense que cette crème est faite pour vous si vous pensez en avoir besoin...",
+      price: "9,90€",
+      stock: "20",
+      imageUrl: "/Gel-douche-burberry-1.jpeg",
+    },
+  ]);
+
+  return (
+    <div>
+      {products.map((product, index) => (
+        <PageProduit key={index} product={product} />
+      ))}
+    </div>
+  );
+}
