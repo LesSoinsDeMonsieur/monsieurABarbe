b743a9ce264b3371e46fc2a9c0e0bdd3a9d6c512
roulios44 <jules.croizier@gmail.com>
2025-05-16 12:01:26 +0200

    fix: header issues for all pages and git issues

--- monsieurABarbeFront/src/app/layout.tsx
+++ monsieurABarbeFront/src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { AuthProvider } from "@/contexts/AuthContext";
 import styles from "./page.module.css";
+import Header from "@/app/header/page";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Header />
         <div className={styles.page}>
           <AuthProvider>{children}</AuthProvider>
         </div>
