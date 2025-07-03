4cee5bc9f0c56887d847f177c9a5cfe8ac0d4ad8
roulios44 <jules.croizier@gmail.com>
2025-07-03 15:30:16 +0200

    Fix responsive footer

--- monsieurABarbeFront/src/app/page.tsx
+++ monsieurABarbeFront/src/app/page.tsx
@@ -32,8 +32,7 @@ export default function Home() {
         <div className={styles.ctas}>
           <a
             className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
+            href="/products"
             rel="noopener noreferrer"
           >
             JE LA VEUX
